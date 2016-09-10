38f08c0b2e21f97a23a26cb8d74a0edc83ddf00d
orhan gazi <orhngazi@gmail.com>
2016-09-10 22:30:37 +0300

    Başlandı
    Projeye başladık. Bismillahirrahmanirrahim

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Orhan Gazi
+ * Date: 10.09.2016
+ * Time: 20:24
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TranslateTo.me</title>
+
+    <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
